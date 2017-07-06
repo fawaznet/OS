665055fb405d8f54b45cf423d3c68872081e33de
Fawaz Alzahrani <fawaz.developer@gmail.com>
2017-07-06 06:51:34 +0300

    Add files via upload

--- interrupts.h
+++ interrupts.h
@@ -3,10 +3,40 @@
 
 #include "types.h"
 #include "ports.h"
+#include "gdt.h"
 
 	class InterruptManager{
+	protected:
+		struct GateDescriptor
+		{
+			uint16_t handlerAddressLowBits;
+			uint8_t gdt_codeSegmentSelector;
+			uint8_t reserved;
+			uint8_t access;
+			uint16_t handlerAddressHighBits
+		}; __attribute__((packed))
+
+		// array with 256 Entries
+		static GateDescriptor interrupDescriptorTable[256];
+
+		static SetinterrupDescriptorTableEntry(
+			uint8_t interruptNumber,
+			uint16_t codeSegmentSelectorOffset,
+			void (*handler)(),
+			uint8_t DescriptorPrivilegeLevel, /*access*/
+			uint8_t DescriptorType /*flags*/
+			);
+
 	public:
+		InterruptManager(GlobalDescriptionTable* gdt);
+		~InterruptManager();
+
 		static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);	//esp is stack pointer
+
+		static void IgnoreInterruptRequest();
+		static void HandleInterruptRequest0x00(); // Timer Interrupt
+		static void HandleInterruptRequest0x01(); // Keyboard Interrupt
+
 	};
 
 
--- interrupts.s
+++ interrupts.s
@@ -1,10 +1,27 @@
+.set IRQ_BASE, 0x20
+
 .section .text
 
 .extern _ZN16InterruptManager15handleInterruptEhj
 
+# exception
+.macro HandleException num
+.global _ZN16InterruptManager16handleException\num\()Ev #puplish outside
+	movb $/num, (interruptNumber)
+	jmp int_bottom
+.endm	#end macro
 
-.macro HandleInterruptRequest interruptNumber
-.global _ZN16InterruptManager26handleInterruptRequest\num\()Ev
+
+# these create an implmintation for functons in interrupts.h
+.macro HandleInterruptRequest num
+.global _ZN16InterruptManager26handleInterruptRequest\num\()Ev #puplish outside
+	movb $/num + IRQ_BASE, (interruptNumber)
+	jmp int_bottom
+.endm	#end macro
+
+#
+HandleInterruptRequest 0x00
+HandleInterruptRequest 0x01
 
 
 
